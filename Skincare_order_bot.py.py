1dbfb0f4e7a021a3d07f69f31ee394f4ef2dff59
Mariluz Chaves <mariluzchavess17@gmail.com>
2023-05-27 15:30:57 -0600

    Create Skincare_order_bot.py.py
    Skincare order chatbot that interacts with users assisting with their skincare orders

---
+++ Skincare_order_bot.py.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+get_ipython().run_cell_magic('writefile', '.env', 'OPENAI_API_KEY= "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"\n')
+
+
+# In[24]:
+
+
+import os
+import openai
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv()) # read local .env file
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+
+# In[25]:
+
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+    )
+    return response.choices[0].message["content"]
+
+def get_completion_from_messages(messages, model="gpt-3.5-turbo", temperature=0):
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, # this is the degree of randomness of the model's output
+    )
+#     print(str(response.choices[0].message))
+    return response.choices[0].message["content"]
+
+
+# In[26]:
+
+
+def collect_messages(_):
+    prompt = inp.value_input
+    inp.value = ''
+    context.append({'role':'user', 'content':f"{prompt}"})
+    response = get_completion_from_messages(context) 
+    context.append({'role':'assistant', 'content':f"{response}"})
+    panels.append(
+        pn.Row('User:', pn.pane.Markdown(prompt, width=600)))
+    panels.append(
+        pn.Row('Assistant:', pn.pane.Markdown(response, width=600, style={'background-color': '#F6F6F6'})))
+ 
+    return pn.Column(*panels)
+
+
+# In[27]:
+
+
+import panel as pn  # GUI
+pn.extension()
+
+panels = [] # collect display 
+
+context = [ {'role':'system', 'content':"""
+You are OrderBot, an automated service to collect orders for a skincare store. \
+You first greet the customer, then collects the order,  \
+and then asks if it's a pickup or delivery. \
+You wait to collect the entire order, then summarize it and check for a final \
+time if the customer wants to add anything else. \
+If it's a delivery, you ask for an address. \
+Finally you collect the payment.\
+Make sure to offer first the product, then when a product is selected you offer the brands \
+identify the item from the menu.\
+You respond in a short, very conversational friendly style. \
+The menu includes \
+Toner: \
+Barr $23.93, Per la Peau $29.09, Murad $30.00, Laneige $33.00 \
+Cleanser: \
+Barr $16.49, CeraVe $15.00, Tatcha $40.00, La Roche Posay $17.00\
+Serum: \
+Estee Lauder $115.00, Barr $28.88, The Ordinary $15.70, Drunk Elephant $38.00 \
+Lip balm: \
+Labello $11.00, Milk $18.00, Coola $18.00, Rhode $16.00 \""""""
+} ]  # accumulate messages
+
+
+inp = pn.widgets.TextInput(value="Hi", placeholder='Enter text here…')
+button_conversation = pn.widgets.Button(name="Chat!")
+
+interactive_conversation = pn.bind(collect_messages, button_conversation)
+
+dashboard = pn.Column(
+    inp,
+    pn.Row(button_conversation),
+    pn.panel(interactive_conversation, loading_indicator=True, height=300),
+)
+
+dashboard
+
+
+# In[ ]:
+
+
+
+
